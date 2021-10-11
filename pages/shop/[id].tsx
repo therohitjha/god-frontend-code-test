e80a2c2474b12f7da1013f2a012764bf8a0e7c19
Rohit Jha <45750703+therohitjha@users.noreply.github.com>
2021-10-11 23:55:53 +0530

    replaced useEffect with getStaticProps

--- pages/shop/[id].tsx
+++ pages/shop/[id].tsx
@@ -1,50 +1,37 @@
-import { useState, useEffect } from "react";
 import CarInfo from "../../src/components/CarInfo";
 import { CarTypes } from "../../types/types";
 import Header from "../../src/components/Header";
-import { useRouter } from "next/router";
+import { server } from "../../public/api/config";
 
 const Shop = ({ data }: { data: CarTypes }) => {
-  
-  const router = useRouter();
-  const [state, setState] = useState<CarTypes>(data);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await fetch(`/api/cars.json`);
-        const data = await res.json();
-        const carInfo = data.find((e: any) => e.id === router.query.id);
-        setState(carInfo);
-      } catch (error) {
-        console.error(error);
-      }
-    })();
-  }, [router]);
-
   return (
     <>
       <Header />
-      <CarInfo
-        data={state}
-        width={800}
-        height={500}
-        isSlider={false}
-      />
+      <CarInfo data={data} width={800} height={500} isSlider={false} />
     </>
   );
 };
 
-Shop.getInitialProps = async () => {
-  return {
-    data: {
-      bodyType: "suv",
-      id: "xc90-recharge",
-      imageUrl: "/images/xc90_recharge.jpg",
-      modelName: "XC90 Recharge",
-      modelType: "plug-in hybrid",
-    },
-  };
-};
+export async function getStaticPaths() {
+  const res = await fetch(`${server}/api/cars.json`);
+  const posts = await res.json();
+
+  const paths = posts.map((post: CarTypes) => ({
+    params: { id: post.id },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const res = await fetch(`${server}/api/cars.json`);
+  const data = await res.json();
+  const carInfo = data.find((e: any) => e.id === id);
+  return { props: { data: carInfo } };
+}
 
 export default Shop;
